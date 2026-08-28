93808d0f3839f7ebe5ed78812a825e83dbe533ea
dalmaktube-creator <dalmaktube@gmail.com>
2026-08-28 16:50:18 +0330

    chore: prepare game services alpha 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.dvgame.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0-alpha01"
+        versionCode = 3
+        versionName = "0.2.0-alpha02"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures { compose = true }
