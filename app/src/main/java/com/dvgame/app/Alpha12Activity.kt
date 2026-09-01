d00321f10047c628f84ef7883a1cb6ef7b2f9e71
Moory Worker <moory@localhost>
2026-09-01 15:04:12 +0000

    chore(ui): prepare subscription onboarding clipboard action

--- app/src/main/java/com/dvgame/app/Alpha12Activity.kt
+++ app/src/main/java/com/dvgame/app/Alpha12Activity.kt
@@ -68,6 +68,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
