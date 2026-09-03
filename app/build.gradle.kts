ebd377be0dc2655bb319832b10084797de4e2a2d
Moory Worker <moory@localhost>
2026-09-03 13:33:55 +0000

    Bump app version to Alpha 15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.dvgame.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.2.0-alpha14"
+        versionCode = 17
+        versionName = "0.2.0-alpha15"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk { abiFilters += "arm64-v8a" }
     }
